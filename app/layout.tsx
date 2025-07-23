3f325811a3eaff8c29f39effd7b5774fbebfa55e
renatopessoa <renato.fjn.si@gmail.com>
2025-07-23 10:54:26 -0300

    fix: corrigir versão do React para 18.3.1 e remover caractere indesejado no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 
 const poppins = Poppins({ 
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800'],¥
+  weight: ['400', '500', '600', '700', '800'],
   variable: '--font-poppins',
   display: 'swap',
 })
